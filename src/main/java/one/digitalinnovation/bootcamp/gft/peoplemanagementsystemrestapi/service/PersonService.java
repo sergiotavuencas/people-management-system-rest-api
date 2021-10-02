c40064f9a6daecfeaf61e115f124b5b4871bb485
Sérgio Vicente T <sergiovicente_tavuenc@outlook.com>
2021-10-01 23:34:34 -0300

    Developed list all method

--- src/main/java/one/digitalinnovation/bootcamp/gft/peoplemanagementsystemrestapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/bootcamp/gft/peoplemanagementsystemrestapi/service/PersonService.java
@@ -8,6 +8,9 @@ import one.digitalinnovation.bootcamp.gft.peoplemanagementsystemrestapi.reposito
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class PersonService {
 
@@ -31,5 +34,14 @@ public class PersonService {
                 .builder()
                 .message("Created person with ID " + savedPerson.getId())
                 .build();
+    }
+
+
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople = personRepository.findAll();
+        return allPeople
+                .stream()
+                .map(personMapper::toDTO)
+                .collect(Collectors.toList());
     }
 }
